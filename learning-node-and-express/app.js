5461b7c36a326e0c208e49a34c588ef1ba1c1bda
anders1216 <anders.hornor@gmail.com>
2019-10-29 15:55:26 -0700

    incorporating server.get and sending a response

--- learning-node-and-express/app.js
+++ learning-node-and-express/app.js
@@ -4,6 +4,10 @@ const PORT = 3000
 
 const server = express();
 
+server.get('/', (req, res) => {
+    res.send("My first route using Express.js!")
+})
+
 server.listen(3000, () => {
 console.log(`Server started on port: ${PORT}`);
 });
