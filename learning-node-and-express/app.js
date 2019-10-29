82c119bf8192c823bcf17215a83091724f670fa6
anders1216 <anders.hornor@gmail.com>
2019-10-29 16:41:37 -0700

    routes with params

--- learning-node-and-express/app.js
+++ learning-node-and-express/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import students from './data/students.json';
+import _ from 'lodash'
 
 const PORT = 3000
 
@@ -9,9 +10,38 @@ const buildURL = (version, path) => {
     return `/api/${version}/${path}`
 }
 
-server.get(buildURL('v1', 'students'), (req, res) => {
+const STUDENT_BASE_URL = buildURL("v1", "students")
+
+server.get(`${STUDENT_BASE_URL}`, (req, res) => {
     res.send(students)
 })
+
+server.get(`${STUDENT_BASE_URL}/:id`, (req, res) => {
+    const student = _.find(students, student => student.id === req.params.id);
+    console.log(student)
+    if (student) {
+        res.json(student);
+    }else{
+        res.send("Student Not Found :?")
+    }
+})
+
+server.post(`${STUDENT_BASE_URL}`, (req, res) => {
+    console.log("handling POST req...")
+    res.end()
+})
+
+server.put(`${STUDENT_BASE_URL}`, (req, res) => {
+    console.log("handling PUT req...")
+    res.end()
+})
+
+server.delete(`${STUDENT_BASE_URL}`, (req, res) => {
+    console.log("handling DELETE req...")
+    res.end()
+})
+
+
 
 server.listen(3000, () => {
 console.log(`Server started on port: ${PORT}`);
